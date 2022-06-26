cfa1379aa6f1fb1ffe2bc9774ea75d11cb1de9c9
Unknower <dev@unknower.com>
2022-06-26 19:36:12 -0400

    publish change

--- src/init.ts
+++ src/init.ts
@@ -3,7 +3,6 @@ import { initImages } from "./images";
 import { initVideoScreens } from "./videos";
 
 export const initScene = (sceneData: any) => {
-  log(sceneData);
   initDialogs(sceneData.dialogs);
   initImages(sceneData.imageTextures);
   initVideoScreens(sceneData.videoSystems);
