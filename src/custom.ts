f4fd5b8fa52d606317ae5be4753d1d2104347ff7
= <=>
2022-07-14 00:27:33 -0400

    add customizations

--- src/custom.ts
+++ src/custom.ts
@@ -1,8 +1,41 @@
-export async function createCustomizations(customizations: any) {
+import { TrackUserFlag } from "@dcl/npc-scene-utils";
+import { TCustomization, TCustomizationFunctions } from "./types/Customization";
+
+const customizationFunctions: TCustomizationFunctions = {
+  create: ([]) => {},
+  update: () => {},
+  remove: () => {}
+};
+
+export function setCustomizations(options: TCustomizationFunctions) {
+  if (options.create) {
+    customizationFunctions.create = options.create;
+  }
+  if (options.update) {
+    customizationFunctions.update = options.update;
+  }
+  if (options.remove) {
+    customizationFunctions.remove = options.remove;
+  }
 }
 
-export function updateCustomization(customizations: any, id: string) {
+export function createCustomizations(customizations: TCustomization[]) {
+  customizationFunctions.create(customizations);
 }
 
-export function removeCustomization(id: string) {
+export function updateCustomization(customizations: TCustomization[], customizationId: string) {
+  let customization;
+  for (let i = 0; i < customizations.length, i++; ) {
+    if (customizations[i].id === customizationId) {
+      customization = customizations[i];
+    }
+  }
+  if (!customization) {
+    return;
+  }
+  customizationFunctions.update(customization, customizationId);
+}
+
+export function removeCustomization(customizationId: string) {
+  customizationFunctions.remove(customizationId);
 }
