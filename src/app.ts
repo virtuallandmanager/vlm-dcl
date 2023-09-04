ee34c64f4ddd328540a6109be8899df84cd12ea3
Unknower <unknower@Unknowers-MBP.attlocal.net>
2023-09-04 18:18:34 -0400

    chore: bump version to 0.0.2

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ import { VLMSceneInitEvent } from "./components/VLMSystemEvents.component";
  * @public
  */
 export abstract class VLM {
-  public static version: string = "0.0.1";
+  public static version: string = "0.0.2";
 
   public static activeServer: string;
 
